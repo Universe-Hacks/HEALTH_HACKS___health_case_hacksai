5112b338d811e9467674072a393a42476aa99250
i.khvorov <i.khvorov@lucky-team.pro>
2023-10-14 18:06:08 +0300

    Cors

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,10 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.routers import build_routers
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 app.include_router(build_routers())
 
 if __name__ == "__main__":
