a70f333128455bc7794aeb079b3a456a3df2a40d
Ilya Zhalnin <pazkal@ro.ru>
2023-10-15 01:42:16 +0300

    Add get endpoint with filter by district id

--- backend/src/depends/city.py
+++ backend/src/depends/city.py
@@ -4,6 +4,7 @@ from bson.errors import InvalidId
 from fastapi import Depends, HTTPException, Path
 
 from src.db.models.city.city_model import CityModel
+from src.db.models.city.district import DistrictModel
 from src.db.repositories.city import InjectCityRepository
 from src.db.types.pydantic_object_id import ObjectId
 
@@ -21,4 +22,23 @@ async def get_city_from_path(
     return city
 
 
+async def get_district_from_path(
+    district_id: Annotated[str, Path()],
+    repo_city: InjectCityRepository,
+) -> DistrictModel:
+    try:
+        city: CityModel | None = await repo_city.find_one(
+            {"districts._id": ObjectId(district_id)}
+        )
+        district = next(filter(lambda d: str(d.id) == district_id, city.districts))
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid city id")
+    if city is None:
+        raise HTTPException(status_code=404, detail="City not found")
+    return district
+
+
 InjectCityFromPath: TypeAlias = Annotated[CityModel, Depends(get_city_from_path)]
+InjectDistrictFromPath: TypeAlias = Annotated[
+    DistrictModel, Depends(get_district_from_path)
+]
